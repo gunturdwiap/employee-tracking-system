9cc9315f7caf7517c1d263bd26a1d85159f96c40
gunturdwiap <kadekguntur13@gmail.com>
2025-01-02 19:03:53 +0800

    update employee schedule & vacation request form ui

--- resources/views/employee/schedule.blade.php
+++ resources/views/employee/schedule.blade.php
@@ -5,44 +5,52 @@
 
     <div
         class="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700 space-y-2">
-        <h5 class="text-xl font-medium text-gray-900 dark:text-white">Hallo bang {{ auth()->user()->name }}</h5>
         <p class=" text-lg font-medium text-gray-900 dark:text-white">
-            Jadwal km:
+            Schedule
         </p>
-        <table class="border">
-            <thead>
-                <tr>
-                    <th>day</th>
-                    <th>start</th>
-                    <th>end</th>
-                    <th>lat long</th>
-                </tr>
-            </thead>
-            <tbody>
-                @forelse ($schedules as $schedule)
-                    <tr class="">
-                        <td>
-                            {{ $schedule->day }}
-                        </td>
-                        <td>
-                            {{ $schedule->work_start_time->format('H:i') }}
-                        </td>
-                        <td>
-                            {{ $schedule->work_end_time->format('H:i') }}
-                        </td>
-                        <td>
-                            {{ $schedule->latitude }}, {{ $schedule->longitude }}
-                        </td>
 
+        <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+            <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                    <tr>
+                        <th scope="col" class="px-6 py-3">
+                            Day
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Start
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            End
+                        </th>
                     </tr>
+                </thead>
+                <tbody>
+                    @forelse ($schedules as $schedule)
+                        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                            <th scope="row"
+                                class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                {{ $schedule->day->label() }}
+                            </th>
+                            <td class="px-6 py-4">
+                                {{ $schedule->work_start_time->format('H:i') }}
+                            </td>
+                            <td class="px-6 py-4">
+                                {{ $schedule->work_end_time->format('H:i') }}
+                            </td>
+                        </tr>
+                    @empty
+                        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                            <th scope="row"
+                                class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                No data
+                            </th>
+                        </tr>
+                    @endforelse
 
-                @empty
-                    <p class=" text-lg font-medium text-gray-900 dark:text-white">
-                        Nggak ada jadwal coy
-                    </p>
-                @endforelse
-            </tbody>
-        </table>
+                </tbody>
+            </table>
+        </div>
+
     </div>
 
 </x-layouts.employee>
--- resources/views/employee/vacation-request.blade.php
+++ resources/views/employee/vacation-request.blade.php
@@ -5,20 +5,39 @@
 
     <div
         class="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700 space-y-2">
-        <h5 class="text-xl font-medium text-gray-900 dark:text-white">Hallo bang {{ auth()->user()->name }}</h5>
+        <h5 class="text-xl font-medium text-gray-900 dark:text-white">Hallo {{ auth()->user()->name }}</h5>
 
-        <form action="" method="post" x-data="{ today: (new Date()).toISOString().split('T')[0] }">
+        <form action="" method="post" x-data="{ today: (new Date()).toISOString().split('T')[0] }" class="grid gap-4">
             @csrf
-            <x-forms.input name="start" type="date" x-bind:min="today"></x-forms.input>
-            <x-forms.error name="start"></x-forms.error>
+            <div>
+                <x-forms.label for="start">
+                    Start date
+                </x-forms.label>
+                <x-forms.input name="start" type="date" x-bind:min="today"></x-forms.input>
+                <x-forms.error name="start"></x-forms.error>
+            </div>
 
-            <x-forms.input name="end" type="date" x-bind:min="today"></x-forms.input>
-            <x-forms.error name="end"></x-forms.error>
+            <div>
+                <x-forms.label for="end">
+                    End date
+                </x-forms.label>
+                <x-forms.input name="end" type="date" x-bind:min="today"></x-forms.input>
+                <x-forms.error name="end"></x-forms.error>
+            </div>
 
-            <textarea name="reason" id="" cols="30" rows="10">{{ old('reason') }}</textarea>
-            <x-forms.error name="reason"></x-forms.error>
+            <div>
+
+                <x-forms.label for="reason">
+                    Reason
+                </x-forms.label>
+                <textarea id="message" rows="4" name="reason"
+                    class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="...">{{ old('reason') }}</textarea>
+                {{-- <textarea name="reason" id="" cols="30" rows="10">{{ old('reason') }}</textarea> --}}
+                <x-forms.error name="reason"></x-forms.error>
+            </div>
             <button
-                class="w-full text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">Submit</button>
+                class="w-full text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Submit</button>
         </form>
     </div>
 
